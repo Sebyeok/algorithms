c17a848a2cd4f658471e29fbfa35901465071f5f
Sebyeok <35682237+Sebyeok@users.noreply.github.com>
2025-09-14 00:37:31 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    long[] dp = new long[91];
+    br.close();
+    dp[1] = 1;
+    dp[2] = 1;
+    
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    System.out.println(dp[n]);
+  }
+}

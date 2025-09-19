10a5c68f53e6bcfcf9155ec0aa298d844ab6fa8c
Sebyeok <35682237+Sebyeok@users.noreply.github.com>
2025-09-19 22:43:57 +0900

    [level 2] Title: 전화번호 목록, Time: 340.57 ms, Memory: 247 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        if(phone_book.length == 1) return false;
+
+        HashSet<String> hs = new HashSet();
+        
+        Arrays.sort(phone_book,(a, b)->a.length()-b.length());
+
+        for(String phone:phone_book){
+            if(!hs.contains(phone)){
+                if(hs.size() > 0){
+                    for(int i = 1; i <= phone.length(); i++){
+                        if(hs.contains(phone.substring(0, i)))
+                            return false;
+                    }
+                }
+                hs.add(phone);
+            } else return false;
+        }
+        return true;
+    }
+}

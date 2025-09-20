87858465602f2096ce01898ab89a1a9188d10dea
Sebyeok <35682237+Sebyeok@users.noreply.github.com>
2025-09-20 11:32:08 +0900

    [level 3] Title: 베스트앨범, Time: 1.60 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        HashMap<String, Integer> genreSumHm = new HashMap();
+        HashMap<String, PriorityQueue<Integer[]>> hm = new HashMap();
+        
+        int n = genres.length;
+        for(int i = 0; i < n; i++){
+            if(hm.get(genres[i]) != null){
+                genreSumHm.put(genres[i], genreSumHm.get(genres[i]) + plays[i]);
+                hm.get(genres[i]).add(new Integer[]{i, plays[i]});
+            } else{
+                genreSumHm.put(genres[i], plays[i]);
+                PriorityQueue<Integer[]> newPq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+                newPq.add(new Integer[]{i, plays[i]});
+                hm.put(genres[i], newPq);
+            }
+        }
+
+        List<String> genreKeyList = new ArrayList<>(genreSumHm.keySet());
+        genreKeyList.sort((a,b)-> genreSumHm.get(b) - genreSumHm.get(a));
+        
+        List<Integer> answerList = new ArrayList<>();
+        
+        for(String genre:genreKeyList){
+            PriorityQueue<Integer[]> queue = hm.get(genre);
+            for(int i=0; i<2; i++){
+                if(!queue.isEmpty())
+                    answerList.add(queue.poll()[0]);
+            }
+        }
+        
+        int[] answer = new int[answerList.size()];
+        for(int i=0;i<answerList.size();i++) answer[i] = answerList.get(i); 
+        
+        return answer;
+    }
+}

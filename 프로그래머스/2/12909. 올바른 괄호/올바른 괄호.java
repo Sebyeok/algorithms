dd33e34d82078682aaf8109826700d39083bc488
Sebyeok <35682237+Sebyeok@users.noreply.github.com>
2025-09-20 13:41:47 +0900

    [level 2] Title: 올바른 괄호, Time: 18.06 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        Stack stack = new Stack();
+        for(int i=0; i<s.length(); i++){
+            char item = s.charAt(i);
+            if(item == '('){
+                stack.push('(');
+            } else{
+                if(stack.size()==0){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        if(stack.size()==0)
+            return true;
+        
+        return false;
+    }
+}

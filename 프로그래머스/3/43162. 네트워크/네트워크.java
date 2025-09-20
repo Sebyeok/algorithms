a46642abb32cc660bfbb371786e49a436332a6be
Sebyeok <35682237+Sebyeok@users.noreply.github.com>
2025-09-20 21:16:02 +0900

    [level 3] Title: 네트워크, Time: 0.84 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public boolean[] visited;
+    public void dfs(int start, int n, int[][] computers){
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.offerFirst(start);
+        while(!stack.isEmpty()){
+            int node = stack.pollFirst();
+            visited[node] = true;
+            for(int i=0; i<n; i++){
+                if(!visited[i] && computers[node][i] == 1){
+                    stack.offerFirst(i);
+                }
+            }
+        }
+    }
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for(int i=0; i<n; i++){
+            if(!visited[i]) {
+                answer++;
+                dfs(i, n, computers);
+            }  
+        }
+        
+        return answer;
+    }
+}
